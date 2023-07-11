f96e41154c1f6bf8d98e6ee07265a6300cc196dc
Maksym Khukhrianskyi <m-k@users.noreply.github.com>
2023-07-11 23:41:33 +0200

    Validate Discount Codes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
-  enum :state, [:open, :completed]
+  enum :state, %i[open completed]
+
+  validates :promotion_codes, promotion_codes: true, allow_nil: true
+  validates :discount_code, inclusion: discounts.keys, allow_nil: true
 end
